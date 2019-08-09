233a35cefe57e04cf553b1d0cb3dd1fa8495c294
yan <gaoy47@126.com>
2019-08-10 00:11:36 +0100

    Change the features.py, optimize the way to get the features, reduce the run time

--- module/features.py
+++ module/features.py
@@ -12,31 +12,36 @@ import numpy as np
 
 import module.content as content
 
-def get_masked_features(mask, img, size):
-	# we can change the size of kernel here
-	# like 1x1, 3x3, 5x5
-	features = []
+# This function will return the mask based on input centre and radius
+# Also, it will return the feature index based on the input parameter, mask index is used for get features
+def get_mask(path, mask_centre, radius, size):
+	print('Pick one slice to get mask and features index')
+	img = cv2.imread(path, -1)
+	height, width = img.shape
+	mask = np.zeros((height, width), np.uint8)
+	cv2.circle(mask, mask_centre, radius, 1, thickness=-1)
+	# Now begin to index the features
+	feature_index = []
 	coordinate = mask.nonzero()
 	for i, j in zip(coordinate[0], coordinate[1]):
 		if size == 3:
-			features.append(img[i-1:i+2, j-1:j+2].ravel())
+			feature_index.append([i-1, i+2, j-1, j+2])
 		elif size == 1:
-			features.append(img[i, j])
+			feature_index.append([i, j])
 		elif size == 5:
-			features.append(img[i-2:i+3, j-2:j+3].ravel())
+			feature_index.append([i-2, i+3, j-2, j+3])
 		else:
-			raise ValueError('Have not assigned such size! Please choose 1, 3 or 5!')
-	return features
+			raise ValueError('Have not input the correct size!')
+	return mask, feature_index
+	
 
-
-def get_all_features_3(path, mask_centre, radius):
-	# To obtain all features, we need to set a circle mask for our data
-	# This is the difference to the previous function
+# This function is used to get all the features for one slice
+def get_all_features_3(path, feature_index):
 	# it will return 3x3x3(x3) data
 	print('Current slice:', path)
 
 	time_slice = os.path.basename(os.path.dirname(path))
-	# get the time stamp for this slice -> to licate its time stamp
+	# get the time stamp for this slice -> to locate its time stamp
 	root_path = os.path.dirname(os.path.dirname(path))
 	# get the root path
 	all_timestamp = content.get_folder(root_path)
@@ -50,14 +55,15 @@ def get_all_features_3(path, mask_centre, radius):
 		target_t_list = [time_slice_index -1, time_slice_index + 1]
 	# this 'if' argument find the previous time stamp and next time stamp for current t
 
+	# get three timestamp: previous, now, future
 	current_path = os.path.dirname(path)
 	previous_path = os.path.join(root_path, all_timestamp[target_t_list[0]])
 	future_path = os.path.join(root_path, all_timestamp[target_t_list[1]])
 
+	# get all the tif content
 	current_all_tif = content.get_allslice(current_path)
 	previous_all_tif = content.get_allslice(previous_path)
 	future_all_tif = content.get_allslice(future_path)
-	# get all the tif content
 
 	# this part of code for x3 space
 	location_slice_index = current_all_tif.index(path)
@@ -84,25 +90,44 @@ def get_all_features_3(path, mask_centre, radius):
 	print('Finished!')
 	# nine images for space + time
 
-	# Here, I create the mask for the data
-	# It needs us to assign centre and radius
-	height, width = img_1.shape
-	mask = np.zeros((height, width), np.uint8)
-	cv2.circle(mask, mask_centre, radius, 1, thickness=-1)
-
 	print('Getting features...')
-	feature_img_1 = np.array(get_masked_features(mask, img_1, 3))
-	feature_img_2 = np.array(get_masked_features(mask, img_2, 3))
-	feature_img_3 = np.array(get_masked_features(mask, img_3, 3))
-	feature_img_4 = np.array(get_masked_features(mask, img_4, 3))
-	feature_img_5 = np.array(get_masked_features(mask, img_5, 3))
-	feature_img_6 = np.array(get_masked_features(mask, img_6, 3))
-	feature_img_7 = np.array(get_masked_features(mask, img_7, 3))
-	feature_img_8 = np.array(get_masked_features(mask, img_8, 3))
-	feature_img_9 = np.array(get_masked_features(mask, img_9, 3))
+	# creat 9 list to store the result
+	feature_img_1 = []
+	feature_img_2 = []
+	feature_img_3 = []
+	feature_img_4 = []
+	feature_img_5 = []
+	feature_img_6 = []
+	feature_img_7 = []
+	feature_img_8 = []
+	feature_img_9 = []
+
+	# feature index has been inputted
+	for i in feature_index:
+		feature_img_1.append(img_1[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_2.append(img_2[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_3.append(img_3[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_4.append(img_4[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_5.append(img_5[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_6.append(img_6[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_7.append(img_7[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_8.append(img_8[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_9.append(img_9[i[0]:i[1], i[2]:i[3]].ravel())
+
+	# transform to the numpy array
+	feature_img_1 = np.array(feature_img_1)
+	feature_img_2 = np.array(feature_img_2)
+	feature_img_3 = np.array(feature_img_3)
+	feature_img_4 = np.array(feature_img_4)
+	feature_img_5 = np.array(feature_img_5)
+	feature_img_6 = np.array(feature_img_6)
+	feature_img_7 = np.array(feature_img_7)
+	feature_img_8 = np.array(feature_img_8)
+	feature_img_9 = np.array(feature_img_9)
+
 	# get all features
 	print('Finished!')
-	print('Concatenating...')
+	print('Concatenating features...')
 	feature_4D = np.concatenate((feature_img_1, feature_img_2, feature_img_3,
                                  feature_img_4, feature_img_5, feature_img_6,
                                  feature_img_7, feature_img_8, feature_img_9), axis=1)
@@ -111,6 +136,7 @@ def get_all_features_3(path, mask_centre, radius):
     
 	return feature_4D, feature_3D
 
+# This function is used to get features from known point -> for evaluation
 def get_assign_features_3(path, x_coordinate, y_coordinate):
 	print('Get 3x3 features')
 	# name_slice = os.path.basename(path)
@@ -180,9 +206,7 @@ def get_assign_features_3(path, x_coordinate, y_coordinate):
 	return feature_4D, feature_3D
 
 
-
-
-def get_all_features_1(path, mask_centre, radius):
+def get_all_features_1(path, feature_index):
 	# it will return 1x1x1(x3) data
 	print('Current slice:', path)
 
@@ -225,19 +249,23 @@ def get_all_features_1(path, mask_centre, radius):
 	# future image
 	# three images for space + time
 
-	# Here, I create the mask for the data
-	# It needs us to assign centre and radius
-	height, width = img_1.shape
-	mask = np.zeros((height, width), np.uint8)
-	cv2.circle(mask, mask_centre, radius, 1, thickness=-1)
-
 	print('Getting features...')
-	feature_img_1 = get_masked_features(mask, img_1, 1)
-	feature_img_2 = get_masked_features(mask, img_2, 1)
-	feature_img_3 = get_masked_features(mask, img_3, 1)
+	feature_img_1 = []
+	feature_img_2 = []
+	feature_img_3 = []
+
+	for i in feature_index:
+		feature_img_1.append(img_1[i[0], i[1]])
+		feature_img_2.append(img_2[i[0], i[1]])
+		feature_img_3.append(img_3[i[0], i[1]])
+	
+	feature_img_1 = np.array(feature_img_1)
+	feature_img_2 = np.array(feature_img_2)
+	feature_img_3 = np.array(feature_img_3)
 	# get all features
 	print('Finished!')
-	print('Concatenating...')
+
+	print('Concatenating features...')
 	feature_4D = np.concatenate((feature_img_1, feature_img_2, feature_img_3), axis=1)
 	feature_3D = np.array(feature_img_1)
 	print('Finished!')
@@ -297,7 +325,7 @@ def get_assign_features_1(path, x_coordinate, y_coordinate):
 	return feature_4D, feature_3D
 
 
-def get_all_features_5(path, mask_centre, radius):
+def get_all_features_5(path, feature_index):
 	# it will return 5x5x5(x3) data
 	print('Current slice:', path)
 
@@ -362,32 +390,63 @@ def get_all_features_5(path, mask_centre, radius):
 	# future tumestamp
 	# nine images for space + time
 
-	# Here, I create the mask for the data
-	# It needs us to assign centre and radius
-	height, width = img_1.shape
-	mask = np.zeros((height, width), np.uint8)
-	cv2.circle(mask, mask_centre, radius, 1, thickness=-1)
 
 	print('Getting features...')
-	feature_img_1 = get_masked_features(mask, img_1, 5)
-	feature_img_2 = get_masked_features(mask, img_2, 5)
-	feature_img_3 = get_masked_features(mask, img_3, 5)
-	feature_img_4 = get_masked_features(mask, img_4, 5)
-	feature_img_5 = get_masked_features(mask, img_5, 5)
-	feature_img_6 = get_masked_features(mask, img_6, 5)
-	feature_img_7 = get_masked_features(mask, img_7, 5)
-	feature_img_8 = get_masked_features(mask, img_8, 5)
-	feature_img_9 = get_masked_features(mask, img_9, 5)
-	feature_img_10 = get_masked_features(mask, img_10, 5)
-	feature_img_11 = get_masked_features(mask, img_11, 5)
-	feature_img_12 = get_masked_features(mask, img_12, 5)
-	feature_img_13 = get_masked_features(mask, img_13, 5)
-	feature_img_14 = get_masked_features(mask, img_14, 5)
-	feature_img_15 = get_masked_features(mask, img_15, 5)
+	# creat 9 list to store the result
+	feature_img_1 = []
+	feature_img_2 = []
+	feature_img_3 = []
+	feature_img_4 = []
+	feature_img_5 = []
+	feature_img_6 = []
+	feature_img_7 = []
+	feature_img_8 = []
+	feature_img_9 = []
+	feature_img_10 = []
+	feature_img_11 = []
+	feature_img_12 = []
+	feature_img_13 = []
+	feature_img_14 = []
+	feature_img_15 = []
+
+	# feature index has been inputted
+	for i in feature_index:
+		feature_img_1.append(img_1[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_2.append(img_2[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_3.append(img_3[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_4.append(img_4[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_5.append(img_5[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_6.append(img_6[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_7.append(img_7[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_8.append(img_8[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_9.append(img_9[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_10.append(img_10[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_11.append(img_11[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_12.append(img_12[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_13.append(img_13[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_14.append(img_14[i[0]:i[1], i[2]:i[3]].ravel())
+		feature_img_15.append(img_15[i[0]:i[1], i[2]:i[3]].ravel())
+
+	# transform to the numpy array
+	feature_img_1 = np.array(feature_img_1)
+	feature_img_2 = np.array(feature_img_2)
+	feature_img_3 = np.array(feature_img_3)
+	feature_img_4 = np.array(feature_img_4)
+	feature_img_5 = np.array(feature_img_5)
+	feature_img_6 = np.array(feature_img_6)
+	feature_img_7 = np.array(feature_img_7)
+	feature_img_8 = np.array(feature_img_8)
+	feature_img_9 = np.array(feature_img_9)
+	feature_img_10 = np.array(feature_img_10)
+	feature_img_11 = np.array(feature_img_11)
+	feature_img_12 = np.array(feature_img_12)
+	feature_img_13 = np.array(feature_img_13)
+	feature_img_14 = np.array(feature_img_14)
+	feature_img_15 = np.array(feature_img_15)
 	# get all features
 	print('Finished!')
-	print('Concatenating...')
 
+	print('Concatenating features...')
 	feature_4D = np.concatenate((feature_img_1,  feature_img_2,  feature_img_3,  feature_img_4,  feature_img_5,
 								 feature_img_6,  feature_img_7,  feature_img_8,  feature_img_9,  feature_img_10,
 								 feature_img_11, feature_img_12, feature_img_13, feature_img_14, feature_img_15), axis=1)
