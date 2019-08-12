13a8e0858e922ebda3d49ac30b1cb17f9b75b365
yan <gaoy47@126.com>
2019-08-12 16:53:09 +0100

    show the selected slice

---
+++ show_single.py
@@ -0,0 +1,115 @@
+'''
+This file will select one time stamp, one slice to show the segmentation result
+
+Author: Yan Gao
+email: gaoy4477@gmail.com
+'''
+
+import cv2
+import os
+import numpy as np 
+import matplotlib.pyplot as plt 
+import module.content as content
+import module.features as features
+from joblib import load
+import argparse
+import time
+
+def get_args():
+	parser = argparse.ArgumentParser(description='Show single results')
+
+	parser.add_argument('--model_4D', nargs="?", type=str, 
+                        help='File name of saved model for 4D data')
+	parser.add_argument('--model_3D', nargs="?", type=str, 
+                        help='File name of saved model for 3D data')
+	parser.add_argument('--size', nargs="?", type=int,
+						help='Size of features, should be 1, 3 or 5')
+	parser.add_argument('--timestamp', nargs="?", type=int,
+						help='Target timestamp')
+	parser.add_argument('--slice', nargs="?", type=int,
+						help='Target slice')
+	args = parser.parse_args()
+	print(args)
+	return args
+
+
+
+args = get_args()
+
+# Here we set the paramater
+mask_centre = (700, 810)
+radius = 550
+
+# get the path for target slice
+current_path = os.getcwd()
+print(current_path)
+all_timestamp = content.get_folder(current_path)
+sub_path = os.path.join(current_path, all_timestamp[args.timestamp])
+sub_all_tif = content.get_allslice(sub_path)
+target_slice = sub_all_tif[args.slice]
+
+# load the model from 'model' folder
+model_4D_path = os.path.join(current_path, 'model', args.model_4D+'.model')
+model_3D_path = os.path.join(current_path, 'model', args.model_3D+'.model')
+model_4D_type = load(model_4D_path)
+model_3D_type = load(model_3D_path)
+
+# get features
+mask, feature_index = features.get_mask(sub_all_tif[0], mask_centre, radius, args.size)
+if args.size == 1:
+	feature_4D, feature_3D = features.get_all_features_1(target_slice, feature_index)
+elif args.size == 3:
+	feature_4D, feature_3D = features.get_all_features_3(target_slice, feature_index)
+elif args.size == 5:
+	feature_4D, feature_3D = features.get_all_features_5(target_slice, feature_index)
+else:
+	raise ValueError('Please input the right size, should be 1, 3 or 5.')
+
+# segment
+prediction_4D = model_4D_type.predict(feature_4D)
+prediction_3D = model_3D_type.predict(feature_3D)
+
+# write the image
+
+coordinate = mask.nonzero()
+# here need to assign the value manually. 
+# Since the classfier will return 0 and 1 randomly
+zero_point_4D_co = np.argwhere(prediction_4D==1)
+# class "1" in 4D model means pore
+zero_point_3D_co = np.argwhere(prediction_3D==1)
+# class "1" in 3D model means pore
+
+height, width = mask.shape
+final_img_4D = np.ones((height,width), np.uint8)
+final_img_3D = np.ones((height,width), np.uint8)
+
+for i in zero_point_4D_co:
+	final_img_4D[coordinate[0][i], coordinate[1][i]] = 0
+for j in zero_point_3D_co:
+	final_img_3D[coordinate[0][j], coordinate[1][j]] = 0
+
+
+# plot the picture
+plt.figure()
+plt.imshow(final_img_4D, 'gray')
+plt.axis('off')
+plt.title('Segment for 4D data')
+
+plt.figure()
+plt.imshow(final_img_3D, 'gray')
+plt.axis('off')
+plt.title('Segment for 3D data')
+
+plt.figure()
+img = cv2.imread(target_slice, -1)
+plt.imshow(img, 'gray')
+plt.axis('off')
+plt.title('Original slice')
+
+plt.show()
+
+
+
+
+
+
