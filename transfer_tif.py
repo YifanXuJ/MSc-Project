5207abeb4822aba87d174d1c44f5d59436116f59
yan <gaoy47@126.com>
2019-08-19 00:48:32 +0100

    change format

---
+++ transfer_tif.py
@@ -0,0 +1,25 @@
+'''
+This file will transfer the .png to .tif under its directory
+
+Author: Yan Gao
+email: gaoy4477@gmail.com
+'''
+from PIL import Image
+import os
+
+path = '/Users/gavin/MSc-Project/SHP15_T113_0025/segmentation_4D'
+
+# find all .png file
+all_files = os.listdir(path)
+all_files.sort()
+all_png = [i for i in all_files if '.png' in i]
+
+save_path = os.path.join(path, 'all_tif')
+if not os.path.exists(save_path):
+	os.mkdir(save_path)
+
+for index, i in enumerate(all_png):
+	if (index+1) % 100 == 0:
+		print(index+1)
+	png = Image.open(os.path.join(path, i))
+	png.save(os.path.join(save_path, i[:-4]))
